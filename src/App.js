c98b1a0640798e336dd3bb7cbec8b94d398c162d
Oscar Pinzon <oskarin31@gmail.com>
2020-12-16 11:15:07 -0500

    add eslint config to project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
 
 function App() {
-  return (
-    <h1>Hello World</h1>
-  );
+  return <h1>Hello World</h1>;
 }
 
 export default App;
