dff606317f82aadffcf1c3885defd004d967f2cf
Oscar Pinzon <oskarin31@gmail.com>
2020-12-18 18:46:58 -0500

    agrega el formulario de registro de alquiler

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
 // Componentes
 import Carro from "./components/Carro";
+import Formulario from "./components/Formulario";
 
-const carros = [
+const testCarros = [
   {
     id: 1,
     precioPorDia: 5,
@@ -72,15 +74,114 @@ const carros = [
   },
 ];
 
+const fechaHoy = new Date().toISOString().slice(0, 10);
+
+const msPorDia = 1000 * 60 * 60 * 24;
+
+const diasMinimoRenta = 1;
+
+function difEnDias(a, b) {
+  // Descarta la informacion de zona horaria y de tiempo.
+  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+  return Math.floor((utc2 - utc1) / msPorDia);
+}
+
+function getFechasIntermedias(start, end) {
+  const startDate = new Date(start);
+  const endDate = new Date(end);
+  for (
+    var arr = [], dt = new Date(startDate);
+    dt <= endDate;
+    dt.setDate(dt.getDate() + 1)
+  ) {
+    arr.push(new Date(dt));
+  }
+  let stringArray = arr.map((v) => v.toISOString().slice(0, 10));
+  return stringArray;
+}
+
 const App = () => {
   const [showModal, setModal] = useState(false);
-  const [selected, setSelected] = useState(carros[0]);
+  const [selected, setSelected] = useState(testCarros[0]);
+  const [carros, setCarros] = useState(testCarros);
+  const [fechaInicioRenta, setFechaInicioRenta] = useState(fechaHoy);
+  const [fechaFinRenta, setFechaFinRenta] = useState(fechaHoy);
+  const [diasRenta, setDiasRenta] = useState(1);
+  const [confirmacionAlquiler, setConfirmacionAlquiler] = useState(false);
+
   const handleClose = () => setModal(false);
+
   const handleSelected = (id) => () => {
-    const carroSeleccionado = carros.filter((carro) => carro.id === id);
-    console.log(carroSeleccionado[0]);
-    setSelected(carroSeleccionado[0]);
+    const carroSeleccionado = carros.find((carro) => carro.id === id);
+    setFechaInicioRenta(fechaHoy);
+    setFechaFinRenta(fechaHoy);
+    setDiasRenta(diasMinimoRenta);
+    setSelected(carroSeleccionado);
+    setConfirmacionAlquiler(false);
     setModal(true);
+  };
+
+  const handleIntentoAlquiler = () => {
+    const fechasSonValidas =
+      new Date(fechaInicioRenta) <= new Date(fechaFinRenta);
+
+    if (fechasSonValidas) {
+      const fechasAlquiladas = getFechasIntermedias(
+        fechaInicioRenta,
+        fechaFinRenta
+      );
+
+      let alquilerLibre = true;
+
+      for (const fecha of fechasAlquiladas) {
+        if (selected.fechasRentadas.includes(fecha)) {
+          alquilerLibre = false;
+          break;
+        }
+      }
+
+      if (alquilerLibre) {
+        const nuevoCarro = { ...selected, fechasRentadas: fechasAlquiladas };
+        setCarros(
+          carros.map((carro) =>
+            carro.id === selected.id ? { ...carro, ...nuevoCarro } : carro
+          )
+        );
+        setConfirmacionAlquiler(true);
+      } else {
+        // TODO: Mostrar mensaje de error
+        console.log("fechas no validas / ya esta alquilado");
+        setConfirmacionAlquiler(false);
+      }
+    } else {
+      // TODO: Mostrar mensaje de error
+      console.log("fechas no validas / date input");
+      setConfirmacionAlquiler(false);
+    }
+  };
+
+  const handleFechaInicioRenta = (event) => {
+    const fechaInicio = new Date(event.target.value);
+    const fechaFin = new Date(fechaFinRenta);
+    if (fechaInicio > fechaFin) {
+      alert("La fecha es invalida");
+    } else {
+      setDiasRenta(difEnDias(fechaInicio, fechaFin) + diasMinimoRenta);
+      setFechaInicioRenta(event.target.value);
+    }
+  };
+
+  const handleFechaFinRenta = (event) => {
+    const fechaInicio = new Date(fechaInicioRenta);
+    const fechaFin = new Date(event.target.value);
+    if (fechaInicio > fechaFin) {
+      alert("La fecha es invalida");
+    } else {
+      setDiasRenta(difEnDias(fechaInicio, fechaFin) + diasMinimoRenta);
+      setFechaFinRenta(event.target.value);
+    }
   };
 
   return (
@@ -100,20 +201,34 @@ const App = () => {
 
       <Modal show={showModal} onHide={handleClose} className="rent-modal">
         <Modal.Header closeButton>
-          <Modal.Title>{selected.nombre}</Modal.Title>
+          <Modal.Title>Rentar {selected.nombre}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>Precio por dia: ${selected.precioPorDia}</p>
-          <p>Pasajeros: {selected.pasajeros}</p>
-          <p>Puertas: {selected.puertas}</p>
-          <p>Transmision: {selected.transmision}</p>
+          {confirmacionAlquiler ? (
+            <Alert className="alert-success" role="alert">
+              <Alert.Heading>Su reserva fue exitosa!</Alert.Heading>
+              <p>Su reserva del {selected.nombre} ha sido completada.</p>
+              <p>
+                Fechas de reserva: {fechaInicioRenta} al {fechaFinRenta}
+              </p>
+              <p>Costo total: ${selected.precioPorDia * diasRenta}</p>
+            </Alert>
+          ) : (
+            <Formulario
+              selected={selected}
+              fechaHoy={fechaHoy}
+              fechaInicioRenta={fechaInicioRenta}
+              handleFechaInicioRenta={handleFechaInicioRenta}
+              fechaFinRenta={fechaFinRenta}
+              handleFechaFinRenta={handleFechaFinRenta}
+              diasRenta={diasRenta}
+              handleIntentoAlquiler={handleIntentoAlquiler}
+            />
+          )}
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
           </Button>
         </Modal.Footer>
       </Modal>
---
+++ src/components/Formulario.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+const Formulario = ({
+  selected,
+  fechaInicioRenta,
+  handleFechaInicioRenta,
+  fechaHoy,
+  fechaFinRenta,
+  handleFechaFinRenta,
+  diasRenta,
+  handleIntentoAlquiler,
+}) => {
+  return (
+    <div>
+      <p>Modelo: {selected.nombre}</p>
+      <p>Precio por dia: ${selected.precioPorDia}</p>
+      <p>Pasajeros: {selected.pasajeros}</p>
+      <p>Puertas: {selected.puertas}</p>
+      <p>Transmision: {selected.transmision}</p>
+      <form>
+        <label>
+          Inicio de la renta:
+          <input
+            type="date"
+            value={fechaInicioRenta}
+            onChange={handleFechaInicioRenta}
+            min={fechaHoy}
+            required
+          ></input>
+        </label>
+        <label>
+          Fin de la renta:
+          <input
+            type="date"
+            value={fechaFinRenta}
+            onChange={handleFechaFinRenta}
+            min={fechaHoy}
+            required
+          ></input>
+        </label>
+        <p>Dias de renta: {diasRenta}</p>
+        <p>Costo de renta: ${diasRenta * selected.precioPorDia}</p>
+        <Button variant="primary" onClick={handleIntentoAlquiler}>
+          Rentar
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default Formulario;
